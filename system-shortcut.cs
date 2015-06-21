38b4d6be51fb15ab7200bef99ec1fce8fc74d5ea
JoaoPedroVieiraLeao <joaopedrovieiraleao@outlook.com>
2015-06-20 22:30:28 -0300

    Update system-shortcut.cs

--- system-shortcut.cs
+++ system-shortcut.cs
@@ -1,7 +1,17 @@
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace sys_shrtcut
 {
-
+  public partial class main(string[] args)
+  {
+    public string _(string v)
+    {
+      return v;
+    }
+    // the system shortcut is a shortucut for open the console
+    Process.Start("app.cs"); // CHANGE TO .exe IF compiled
+    Console.write(_("PROCESS STARTED WITH SUCESS"));
+  }
 }
